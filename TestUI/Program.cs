a5292bf8f4adb66ebaf6dd82b077df75a837a059
Leonardo Vargas <layonard@gmail.com>
2022-11-13 12:02:54 -0400

    Create solution and projects

---
+++ TestUI/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please enter your name");
+string firstName = Console.ReadLine();
+
+Console.WriteLine($"Hello {firstName}");
